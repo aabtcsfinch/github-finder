221e7938042ea5812a7337868aa498f7f18860d0
carl <carl@localhost-live.lan>
2022-11-12 14:43:24 -0500

    setup tailwind & daisyui and setting up initial flex grid layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div>
+    <h1 >Hello World!</h1>
+  </div>
   );
 }
 
